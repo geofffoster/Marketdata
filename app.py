cd03bd0cdabdfb81d4b164d77777e12dafd75b39
geoff <fostergeoff@yahoo.com>
2022-09-09 16:01:29 +0100

    again

--- app.py
+++ app.py
@@ -12,19 +12,23 @@ import streamlit as st
 st.title('Selected Market Prices.')
 
 
-tab1, tab2, tab3 = st.tabs(["Currency Pairs NZ", "Currency Pairs UK", "Oil & Gas"])
+tab1, tab2, tab3, tab4 = st.tabs(["Currency Pairs NZ", "Currency Pairs UK", "Oil & Gas", "Ruble"])
 
 
 with tab1:
     st.header("Currency Pairs NZ")
-    tickers =['GBPNZD=X', 'USDNZD=X', 'AUDNZD=X']
+    tickers =['GBPNZD=X', 'USDNZD=X', 'AUDNZD=X', 'NZDJPY=X']
     currency=tickers
     start='2020-01-01'
     end=date.today()
 
     df =  yf.download(tickers, start, end)['Adj Close']
-    fig = df.iplot(asFigure=True, xTitle="Time",
-            yTitle="Prices", title="Currency Prices")
+    # fig = df.iplot(asFigure=True, xTitle="Time",
+    #         yTitle="Prices", title="Currency Prices")
+
+    fig = go.Figure(**tools.merge_figures([df.figure(columns=['GBPNZD=X', 'USDNZD=X', 'AUDNZD=X']), df.figure(columns=['NZDJPY=X'], color='red')])).set_axis(['NZDJPY=X'], side='right')
+    fig['layout'].update(title='Currency Prices')
+
     st.plotly_chart(fig, use_container_width=True)
 
 with tab2:
@@ -51,3 +55,15 @@ with tab3:
     fig3 = go.Figure(**tools.merge_figures([df.figure(columns=['NG=F']), df.figure(columns=['CL=F'], color='green')])).set_axis(['CL=F'], side='right')
     fig3['layout'].update(title='Oil and Gas Prices')
     st.plotly_chart(fig3, use_container_width=True)
+
+with tab4:
+    st.header("Russian Ruble vs USD")
+    tickers = tickers = ['RUBUSD=X']
+    currency=tickers
+    start='2020-01-01'
+    end=date.today()
+
+    df =  yf.download(tickers, start, end)['Adj Close']
+    fig4 = df.iplot(asFigure=True, xTitle="Time",
+            yTitle="Prices", title="Currency Prices")
+    st.plotly_chart(fig4, use_container_width=True)   
