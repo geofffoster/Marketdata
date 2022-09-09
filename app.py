ef8ebda2cb7289df5f6be01ab2d9b0b3970ae701
geoff <fostergeoff@yahoo.com>
2022-09-09 17:57:43 +0100

    again

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ from datetime import date
 import yfinance as yf
 import numpy as np
 import cufflinks as cf
-cf.go_offline()
 from cufflinks import tools
+cf.go_offline()
 import plotly.graph_objs as go
+import wbgapi as wb
+
 import streamlit as st
 
 st.title('Selected Market Prices.')
 
 
-tab1, tab2, tab3, tab4 = st.tabs(["Currency Pairs NZ", "Currency Pairs UK", "Oil & Gas", "Ruble"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Currency Pairs NZ", "Currency Pairs UK", "Oil & Gas", 
+    "Ruble", "Interest Rates", "GDP per Capita"])
 
 
 with tab1:
@@ -64,6 +67,21 @@ with tab4:
     end=date.today()
 
     df =  yf.download(tickers, start, end)['Adj Close']
-    fig4 = df.iplot(asFigure=True, xTitle="Time",
-            yTitle="Prices", title="Currency Prices")
+    fig4 = df.iplot(asFigure=True, xTitle="Time", yTitle="Prices", title="Currency Prices")
     st.plotly_chart(fig4, use_container_width=True)   
+
+with tab5:
+    st.header("Inflation Rates")
+    data = pd.read_html('https://en.wikipedia.org/wiki/List_of_countries_by_inflation_rate')
+    table = data[1].set_index('Country (or Territory)')['Inflation rate (consumer prices) (%)']
+    array = ['Australia', 'New Zealand', 'Austria', 'Germany', 'France', 'United Kingdom', 'Spain', 'Portugal', 'United States', 'Russia', 'Italy', 'Netherlands', 'Sweden']
+    df = table.reset_index().loc[table.reset_index()['Country (or Territory)'].isin(array)]
+    df = df.set_index('Country (or Territory)')
+    fig5 = df['Inflation rate (consumer prices) (%)'].iplot(asFigure=True, kind='bar', title='Inflation Rates  2021')
+    st.plotly_chart(fig5, use_container_width=True)   
+
+with tab6:
+    st.header("Inflation Rates")
+    gdppercap=wb.data.DataFrame('NY.GDP.PCAP.CD', wb.region.members('EMU'))
+    fig6 = gdppercap.iloc[:,-6:].iplot(asFigure=True, kind='bar', title='GDP per Capita in Europe')
+    st.plotly_chart(fig6, use_container_width=True)   
