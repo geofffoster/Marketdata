1256a7ec0942ac1b17a0a82aa4211466a433ac77
geoff <fostergeoff@yahoo.com>
2022-09-09 15:31:29 +0100

    again

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ tab1, tab2 = st.tabs(["Currencies", "Oil & Gas"])
 
 
 with tab1:
-    st.header("Currencies")
+    st.header("Currency Pairs NZ")
     tickers =['GBPNZD=X', 'USDNZD=X', 'AUDNZD=X']
     currency=tickers
     start='2020-01-01'
@@ -28,8 +28,21 @@ with tab1:
             yTitle="Prices", title="Currency Prices")
     st.plotly_chart(fig, use_container_width=True)
 
-
 with tab2:
+    st.header("Currency Pairs UK")
+    tickers =['['EURUSD=X', 'GBPEUR=X',  'GBPUSD=X']
+    currency=tickers
+    start='2020-01-01'
+    end=date.today()
+
+    df =  yf.download(tickers, start, end)['Adj Close']
+
+    fig = df.iplot(asFigure=True, xTitle="Time",
+            yTitle="Prices", title="Currency Prices")
+    st.plotly_chart(fig, use_container_width=True)    
+
+
+with tab3:
     st.header("Oil (CL=F) & Gas (NG=F")
     tickers = ['NG=F', 'CL=F']
     currency=tickers
@@ -37,6 +50,6 @@ with tab2:
     end=date.today()
 
     df =  yf.download(tickers, start, end)['Adj Close']
-    fig2 = go.Figure(**tools.merge_figures([df.figure(columns=['NG=F']), df.figure(columns=['CL=F'], color='green')])).set_axis(['CL=F'], side='right')
-    fig2['layout'].update(title='Oil and Gas Prices')
-    st.plotly_chart(fig2, use_container_width=True)
+    fig3 = go.Figure(**tools.merge_figures([df.figure(columns=['NG=F']), df.figure(columns=['CL=F'], color='green')])).set_axis(['CL=F'], side='right')
+    fig3['layout'].update(title='Oil and Gas Prices')
+    st.plotly_chart(fig3, use_container_width=True)
